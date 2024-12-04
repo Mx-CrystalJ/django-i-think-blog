55bcc929c9d9f7ad8cce10da10f01802ea4bd763
Crystal Johnson <mxcrystaljohnson@gmail.com>
2024-12-04 15:47:34 +0000

    secret key visible to everyone

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*k3e+#kaxuu03@_1fi2qcsi6+yke$j8$hox599p$1@b3pls7bv'
-SECRET_KEY = os.environ.get("kl(3.>rq0z}771o0oyMK%rs{R}(RgW")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
