6b88f4aea0cb6261089fda73ee16548fa835d9c2
Crystal Johnson <mxcrystaljohnson@gmail.com>
2024-12-05 18:26:49 +0000

    Fix comment model bug, no migrate

--- blog/models.py
+++ blog/models.py
@@ -18,14 +18,13 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter", default=1)
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
+    approved = models.BooleanField(default=False)
 
 
 
