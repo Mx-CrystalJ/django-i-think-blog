2e1320f6561920baa12fd45335fe368611b515a2
Crystal Johnson <mxcrystaljohnson@gmail.com>
2024-12-10 21:14:29 +0000

    fix migrate files

--- about/models.py
+++ about/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class About(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()  # For longer text descriptions
     updated_on = models.DateTimeField(auto_now=True)
     # Add other relevant fields like images, authors, etc.
